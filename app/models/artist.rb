caec11b4d9959b946f294d50e23ea189ea5e5edc
alexneustein <alex.neustein@gmail.com>
2018-09-06 17:26:01 -0400

    Added full name artist model method and routes.db

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -2,4 +2,9 @@ class Artist < ApplicationRecord
   has_many :images
   has_many :messages
   has_and_belongs_to_many :types
+
+  def full_name
+    "#{self.name_first} #{self.name_last}"
+  end
+
 end
