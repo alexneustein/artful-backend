7a617790c911099f8caabffd713b86482de83d92
alexneustein <alex.neustein@gmail.com>
2018-09-11 14:53:21 -0400

    added image url

--- app/serializers/image_serializer.rb
+++ app/serializers/image_serializer.rb
@@ -1,4 +1,4 @@
 class ImageSerializer < ActiveModel::Serializer
-  attributes :id, :artist_id, :image_date, :details, :imagedata, :title, :likes
+  attributes :id, :artist_id, :image_date, :details, :imagedata, :title, :likes, :url
   has_many :comments
 end
