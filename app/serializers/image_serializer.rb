de8cdb77b4b72760ab5d6cfe80aae7a98d1922a2
alexneustein <alex.neustein@gmail.com>
2018-09-12 09:48:34 -0400

    updated serializers

--- app/serializers/image_serializer.rb
+++ app/serializers/image_serializer.rb
@@ -1,4 +1,13 @@
 class ImageSerializer < ActiveModel::Serializer
-  attributes :id, :artist_id, :image_date, :details, :imagedata, :title, :likes, :url
-  belongs_to :artist
+  attributes :id, :artist_id, :image_date, :details, :imagedata, :title, :likes, :url, :artist
+  # belongs_to :artist
+
+
+def artist
+    # object.artists.map do |artist|
+    #   ::ProjectSerializer.new(artist).attributes
+    # end
+
+    object.artist.full_name
+  end
 end
